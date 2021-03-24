e1f8dd5ac343ac38d05faa5d03493a1e08721397
Colton Voege <cvoege+nix@gmail.com>
2021-03-24 10:11:25 -0400

    first commit

---
+++ create.sh
@@ -0,0 +1,1 @@
+rails new . --api --force --skip-bundle --skip-action-mailer --skip-puma --skip-action-cable --skip-sprockets --skip-spring --skip-listen --skip-javascript --skip-turbolinks --skip-jbuilder --skip-bootsnap -m https://www.graphiti.dev/template .
